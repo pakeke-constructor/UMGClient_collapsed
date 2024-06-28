1c2b555a6f7d75d22bc36b9fb31d8fbf64c6ec36
Oli <OliverWDGarrett@gmail.com>
2024-06-28 16:29:33 +1200

    Started on physics menu

--- lootplot/states/MenuElement.lua
+++ lootplot/states/MenuElement.lua
@@ -10,6 +10,78 @@ local MenuElement = LUI.Element()
 
 
 
+local intersect = require("libs.nm_batteries.intersect")
+
+local AutoAtlas = require("libs.AutoAtlas.AutoAtlas")
+
+local GRAVITY = 100
+local DIR = "lootplot/assets/items/"
+
+local NUM_ITEMS = 400
+
+local WORLD_WIDTH, WORLD_HEIGHT = 800,600
+
+
+
+local PhysicsItem = tools.SafeClass()
+
+local ITEM_SHAPE = love.physics.newCircleShape(10)
+
+function PhysicsItem:init(world, quad)
+    self.quad = quad
+    -- In the future, have deterministic positioning.
+    -- Perhaps a grid layout?
+    local x,y = love.math.random(0,WORLD_WIDTH), love.math.random(0,WORLD_HEIGHT)
+    local body = love.physics.newBody(world, x,y)
+    self.fixture = love.physics.newFixture(body, ITEM_SHAPE)
+end
+
+function PhysicsItem:update(dt)
+end
+
+function PhysicsItem:draw(atlas)
+    atlas:draw(self.quad)
+end
+
+
+
+
+
+
+local function loadAtlas()
+    local atlas = AutoAtlas()
+    local quads = tools.Array()
+
+    local t = love.filesystem.getDirectoryItems(DIR)
+    for _, file in ipairs(t) do
+        local img = love.image.newImageData(DIR..file)
+        local q = atlas:add(img)
+        quads:add(q)
+    end
+    return atlas, quads
+end
+
+
+local function setup(self)
+    local quads
+    self.atlas, quads = loadAtlas()
+    self.world = love.physics.newWorld(0,GRAVITY)
+    self.items = tools.Array()
+
+    for _ = 1, NUM_ITEMS do
+        local q = table.pick_random(quads)
+        self.items:add(PhysicsItem(self.world, q))
+    end
+end
+
+local function free(self)
+    -- Clears all references to images and stuff
+    self.atlas = nil
+    self.items = nil
+    self.world = nil
+end
+
+
 
 function MenuElement:init(options)
     tools.assertKeys(options, {"onPlay"})
