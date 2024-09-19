e8fb8a50ee0a4b4018d8488c2a5ac99395182250
Miku AuahDark <lumi.hotaru@gmail.com>
2024-09-19 09:55:54 +0800

    Expand directoryObject functions.

--- src/common/api/umg/directoryObjects.lua
+++ src/common/api/umg/directoryObjects.lua
@@ -30,6 +30,11 @@ local function newDirObj(originalFsysobj, append_path)
         return fsysObj:getDirectoryItems(dir)
     end
 
+    ---@param dir string
+    function self:createDirectory(dir)
+        return fsysObj:createDirectory(dir)
+    end
+
     ---@param fname string
     ---@return string?,string?
     function self:read(fname)
@@ -38,8 +43,48 @@ local function newDirObj(originalFsysobj, append_path)
     end
 
     ---@param fname string
+    ---@param data string|love.Data
+    ---@param size integer?
+    ---@return boolean,string?
+    function self:write(fname, data, size)
+        return fsysObj:write(fname, data, size)
+    end
+
+    ---@param fname string
+    ---@param data string|love.Data
+    ---@param size integer?
+    ---@return boolean,string?
+    function self:append(fname, data, size)
+        return fsysObj:append(fname, data, size)
+    end
+
+    ---@param fname string
+    ---@param mode love.FileMode
+    ---@return (love.File)?,string?
+    function self:openFile(fname, mode)
+        return fsysObj:openFile(fname, mode)
+    end
+
+    ---@param fname string
     function self:newFileData(fname)
         return fsysObj:newFileData(fname)
+    end
+
+    ---@param path string
+    ---@return boolean
+    function self:exists(path)
+        return fsysObj:exists(path)
+    end
+
+    ---@param path string
+    ---@return boolean
+    function self:remove(path)
+        return fsysObj:remove(path)
+    end
+
+    ---@return boolean
+    function self:isWritable()
+        return fsysObj:isWritable()
     end
 
     return self
